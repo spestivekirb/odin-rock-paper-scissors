31e1cdae03d58c393d16f10b75a891b5fdb1300a
Jerry Wong <187459546+spestivekirb@users.noreply.github.com>
2026-01-08 22:43:29 -0500

    Create computer choice logic
    Created getComputerChoice() function to randomly generate computer's
    move, with equal probability.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+// Step 2: Computer choice logic:
+
+// Generate a random number [0, 1)
+// Multiply number by 3
+// IF number range [0, 1) return "rock"
+// ELSE IF number range [1, 2) return "paper"
+// ELSE return "scissors"
+
+function getComputerChoice() {
+    const random = Math.random() * 3;
+    if (random < 1) {
+        return "rock";
+    } else if (random < 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+//Debug
+console.log(getComputerChoice());
